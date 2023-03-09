6d975a927b471166529625446747d7b508e77034
DeveloperJavier13 <73495101+DeveloperJavier13@users.noreply.github.com>
2023-03-09 11:16:57 -0600

    muestraTablas

---
+++ wwwroot/tabla.js
@@ -0,0 +1,22 @@
+var btnConsultarAreas = document.querySelector('#btnConsultarTabla');
+console.log('entra al metodo');
+btnConsultarAreas.addEventListener('click', () => {
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+            let dataSetEgresos =[];
+                console.log({data});
+            data.forEach(registro => {
+                dataSetEgresos.push(Object.values(registro));
+            });
+
+            let tablasArea = $('#areas-table').DataTable({
+                data: dataSetEgresos,
+                columns: [
+                    {title: "Egreso"},
+                    {title: "Concepto"},
+                ],
+                "bDestroy": true,
+            });
+        });
+});
